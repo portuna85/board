35e308fa642953f76f979455e3659a5b9837a3a9
portuna85 <portuna85@gmail.com>
2022-11-10 15:03:15 +0900

    로그인 후 10분동안 이벤트 없을시 접속 종료

--- src/main/java/com/blitz/board/web/HomeController.java
+++ src/main/java/com/blitz/board/web/HomeController.java
@@ -1,23 +1,18 @@
 package com.blitz.board.web;
 
 import com.blitz.board.domain.User;
-import com.blitz.board.repository.UserRepository;
 import com.blitz.board.web.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-
+import javax.servlet.http.Cookie;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-
-    private final UserRepository userRepository;
-
 
     @GetMapping("/")
     public String index(@SessionAttribute(value = SessionConst.LOGIN_USER, required = false) User loginUser, Model model) {
@@ -29,6 +24,8 @@ public class HomeController {
 
         // Session이 유지되면 로그인으로 이동
         model.addAttribute("user", loginUser);
+
+
         return "loginHome";
     }
 }
