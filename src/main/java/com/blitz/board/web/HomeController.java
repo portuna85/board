a7d7291cc06b0fcad8b3ff9b2d9b2741f577b6bb
portuna85 <portuna85@gmail.com>
2022-11-10 11:21:12 +0900

    로그인 후 Jsessionid 노출 삭제

--- src/main/java/com/blitz/board/web/HomeController.java
+++ src/main/java/com/blitz/board/web/HomeController.java
@@ -1,35 +1,27 @@
 package com.blitz.board.web;
 
 import com.blitz.board.domain.User;
-import com.blitz.board.web.session.SessionManager;
+import com.blitz.board.web.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final SessionManager sessionManager;
-
-    // @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model) {
+    public String index(@SessionAttribute(value = SessionConst.LOGIN_USER, required = false) User loginUser, Model model) {
 
-        User user = (User) sessionManager.getSession(request);
-
-        if (user == null) {
+        // 세션에 회원 데이터가 없으면 home
+        if (loginUser == null) {
             return "index";
         }
 
-        model.addAttribute("user", user);
+        // Session이 유지되면 로그인으로 이동
+        model.addAttribute("user", loginUser);
         return "loginHome";
     }
 }
